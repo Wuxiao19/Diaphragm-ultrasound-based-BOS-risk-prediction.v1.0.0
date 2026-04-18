a05d614eab7f95812ef4ffd463c473eddc296998
Wuxiao19 <wux3196737594@gmail.com>
2026-04-18 14:55:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,10 @@ st.set_page_config(
 st.title(
     "Diaphragm Ultrasound Analysis System"
     help=(
-"Upload **B-mode** and **M-mode** diaphragm ultrasound images"
-"for one patient (single exam) or for multiple patients (batch exams)."
-
-"The system will automatically perform: feature extraction → feature reduction"
-"→ feature fusion → ExtraTrees-based binary classification.  "
+        "Upload **B-mode** and **M-mode** diaphragm ultrasound images"
+        "for one patient (single exam) or for multiple patients (batch exams)."
+        "The system will automatically perform: feature extraction → feature reduction"
+        "→ feature fusion → ExtraTrees-based binary classification.  "
     )
 )
 
